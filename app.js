55921f6f7822be5fc0f29124fb6c35da916a9d34
Anssi Kinnunen <anssi.kinnunen@poutapilvi.fi>
2016-07-15 13:20:30 +0300

    use correct port

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 	res.json({ text: 'Hello, world!' });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Example app listening on port 3000!');
 });
