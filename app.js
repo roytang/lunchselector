239c4e75528da8b98ec908dbad3bc3911de20f0f
Anssi Kinnunen <anssi.kinnunen@poutapilvi.fi>
2016-07-15 13:32:56 +0300

    add post route for testing

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 let port = process.env.PORT || 3000;
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.json({ text: 'Hello, world!' });
+});
+
+app.post('/', (req, res) => {
+	let text = req.body.text;
+	console.log(`DEBUG: got post on '/', text: ${text}`);
 });
 
 app.listen(port, () => {
